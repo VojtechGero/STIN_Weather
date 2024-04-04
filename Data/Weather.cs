5d79fbfa6ea961f374536b97ddc3900e3273dbb1
Vojtěch Gerö <vojtech.gero@tul.cz>
2024-04-04 12:14:14 +0200

    added weather api infrastructure

---
+++ Data/Weather.cs
@@ -0,0 +1,18 @@
+﻿namespace STIN_Weather.Data
+{
+    public class Weather
+    {
+        public string json {  get; set; }
+
+        Weather(double latitude, double longitude)
+        {
+
+        }
+
+        public Weather requestWeather(double? latitude, double? longitude)
+        {
+            if (latitude is null or longitude is null) return null;
+            return new Weather(latitude, longitude);
+        }
+    }
+}
---
+++ Data/WeatherApi.cs
@@ -0,0 +1,20 @@
+﻿namespace STIN_Weather.Data
+{
+    public class WeatherApi
+    {
+        private static HttpClient sharedClient = new()
+        {
+            BaseAddress = new Uri("https://api.open-meteo.com/v1/forecast"),
+        };
+
+        static async Task<string> GetAsync(HttpClient httpClient)
+        {
+            using HttpResponseMessage response = await httpClient.GetAsync("");
+
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadAsStringAsync();
+             
+        }
+    }
+}
