06138c1d28acbe2367fa2ebd59032a5e91b23f50
VojtechGero <vojtech.gero@tul.cz>
2024-04-06 14:09:44 +0200

    saved it

---
+++ WeatherReportUtils/RequestBuilder.cs
@@ -0,0 +1,28 @@
+﻿using STIN_Weather.Data;
+
+namespace STIN_Weather.WeatherReportUtils;
+
+public class RequestBuilder
+{
+    private string request;
+    public RequestBuilder(Coordinates coords)
+    {
+        request = $"?latitude={coords.latitude}&longitude={coords.longitude}";
+    }
+    public void DailyTemperatureMax()
+    {
+        request += "&daily=temperature_2m_max";
+    }
+    public void DailyWeatherCode()
+    {
+        request += "&daily=weather_code";
+    }
+    public void DailyPrecipitationSum()
+    {
+        request += "&daily=precipitation_sum";
+    }
+    public string GetRequest()
+    {
+        return request;
+    }
+}
--- WeatherReportUtils/WeatherApi.cs
+++ WeatherReportUtils/WeatherApi.cs
@@ -1,21 +1,34 @@
-﻿namespace STIN_Weather.WeatherReportUtils
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using STIN_Weather.Data;
+
+namespace STIN_Weather.WeatherReportUtils;
+
+public class WeatherApi
 {
-    public class WeatherApi
+    private HttpClient client = new()
     {
-        private HttpClient client = new()
+        BaseAddress = new Uri("https://api.open-meteo.com/v1/forecast"),
+    };
+
+    private async Task<string> GetAsync(string request)
+    {
+        using HttpResponseMessage response = await client.GetAsync(request);
+        response.EnsureSuccessStatusCode();
+        return await response.Content.ReadAsStringAsync();
+    }
+
+    public async Task<WeatherData> requestWeather(string request)
+    {
+        try
         {
-            BaseAddress = new Uri("https://api.open-meteo.com/v1/forecast"),
-        };
-
-        public async Task<string> GetAsync(double latitude, double longitude)
+            string json = await GetAsync(request);
+            var we = JsonSerializer.Deserialize<WeatherData>(json, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+            return we;
+        }
+        catch(HttpRequestException)
         {
-            string request = $"?latitude={latitude}&longitude={longitude}";
-            using HttpResponseMessage response = await client.GetAsync(request);
-
-            response.EnsureSuccessStatusCode();
-
-            return await response.Content.ReadAsStringAsync();
-
+            throw;
         }
     }
 }
