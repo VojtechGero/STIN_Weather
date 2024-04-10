7b6bd3f5f203ddfa22bf6da7bcc407e85f7818af
Vojtěch Gerö <vojtech.gero@tul.cz>
2024-04-10 11:54:02 +0200

    Added location saving UI

--- Data/SavedLocation.cs
+++ Data/SavedLocation.cs
@@ -2,9 +2,16 @@
 {
     public class SavedLocation
     {
-        double latitude;
-        double longitude;
-        string name;
-        int id;
+        public double latitude {  get; set; }
+        public double longitude { get; set; }
+        public string name { get; set; }
+        public int id { get; set; }
+
+        public SavedLocation(Coordinates coords, string name)
+        {
+            this.latitude = coords.latitude;
+            this.longitude = coords.longitude;
+            this.name = name;
+        }
     }
 }
