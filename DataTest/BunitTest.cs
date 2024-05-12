d3f664ad2661abec5f3f55c7b6fa01c4e947021c
VojtechGero <vojtech.gero@tul.cz>
2024-05-12 13:58:57 +0200

    removed code

---
+++ DataTest/BunitTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Bunit;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using STIN_Weather.Components.Pages;
+using STIN_Weather.Data;
+namespace Tests;
+
+[TestClass]
+public class BunitTest :Bunit.TestContext
+{
+    /*
+    [TestMethod]
+    public async Task OnInitializedAsync_UserExists_LocationsSet()
+    {
+        // Arrange
+        var user = new ApplicationUser();
+        user.savedLocations = new List<SavedLocation> { new SavedLocation(), new SavedLocation() };
+
+        var authState = new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity(new[] { new Claim(ClaimTypes.NameIdentifier, "testuser") })));
+        var authStateProviderMock = new Mock<AuthenticationStateProvider>();
+        authStateProviderMock.Setup(m => m.GetAuthenticationStateAsync()).ReturnsAsync(authState);
+
+        var userManagerMock = new Mock<UserManager<ApplicationUser>>(MockBehavior.Strict);
+        userManagerMock.Setup(m => m.GetUserAsync(authState.User)).ReturnsAsync(user);
+
+        var services = new ServiceCollection();
+        services.AddSingleton(authStateProviderMock.Object);
+        services.AddSingleton(userManagerMock.Object);
+
+        Services.AddSingleton(authStateProviderMock.Object);
+        Services.AddSingleton(userManagerMock.Object);
+        var cut = Render<Home>();
+
+        // Act
+
+        // Assert
+        Assert.AreEqual(2, cut.Instance.locations.Count); // Ensure locations were set
+                                                          // Add more assertions as needed
+    }
+    */
+}
--- STIN_Weather/Components/Account/IdentityRedirectManager.cs
+++ STIN_Weather/Components/Account/IdentityRedirectManager.cs
@@ -32,28 +32,7 @@ namespace STIN_Weather.Components.Account
             throw new InvalidOperationException($"{nameof(IdentityRedirectManager)} can only be used during static rendering.");
         }
 
-        [DoesNotReturn]
-        public void RedirectTo(string uri, Dictionary<string, object?> queryParameters)
-        {
-            var uriWithoutQuery = navigationManager.ToAbsoluteUri(uri).GetLeftPart(UriPartial.Path);
-            var newUri = navigationManager.GetUriWithQueryParameters(uriWithoutQuery, queryParameters);
-            RedirectTo(newUri);
-        }
 
-        [DoesNotReturn]
-        public void RedirectToWithStatus(string uri, string message, HttpContext context)
-        {
-            context.Response.Cookies.Append(StatusCookieName, message, StatusCookieBuilder.Build(context));
-            RedirectTo(uri);
-        }
 
-        private string CurrentPath => navigationManager.ToAbsoluteUri(navigationManager.Uri).GetLeftPart(UriPartial.Path);
-
-        [DoesNotReturn]
-        public void RedirectToCurrentPage() => RedirectTo(CurrentPath);
-
-        [DoesNotReturn]
-        public void RedirectToCurrentPageWithStatus(string message, HttpContext context)
-            => RedirectToWithStatus(CurrentPath, message, context);
     }
 }
